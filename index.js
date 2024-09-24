943148d9c3474a52bd438c55fd8658695644424e
Weronika Niemczyk <112780270+WeronikaNiemczyk@users.noreply.github.com>
2024-09-24 15:29:02 +0200

    update style

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ buttons.forEach((button) => {
     collapsible.classList.toggle("active");
   });
 });
+
+const sections = document.querySelectorAll(".toggle-section");
+
+sections.forEach((section) => {
+  section.addEventListener("click", function () {
+    const collapsible = this.parentElement;
+    collapsible.classList.toggle("active");
+  });
+});
